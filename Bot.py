f239f36b72811575a5b7e814e9839e5bf63b74c8
Dean Carey <145177522+dcarey2025@users.noreply.github.com>
2023-12-01 15:58:27 -0500

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,10 +1,28 @@
 from Chessboard import ChessBoard
 from Chesspiece import Chesspiece
 
+self.isWhite = true
 self.score = 0
 self.board = ChessBoard() #generate fresh board
 
 # Best fit heuristic to evaluate numerical position
-def update(self,board):
-    self.score = board.evaluateMaterial()
-    
+def calcScore(self, board):
+    self.score = board.evaluateMaterial() # counts taken piece values
+
+    if (self.board.isBlackInCheck(self)):
+        if self.isWhite:
+            self.score *= 1.25
+        else:
+            self.score *= 0.75
+            
+
+    if (self.board.isWhiteInCheck(self)):
+        if !self.isWhite:
+            self.score *= 1.25
+        else:
+            self.score *= 0.75
+
+     
+
+    return score
+
