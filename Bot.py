b9e627933944adfb83bc1f4311a57d12732427aa
Dean Carey <145177522+dcarey2025@users.noreply.github.com>
2023-12-01 15:37:43 -0500

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,3 +1,10 @@
 from Chessboard import ChessBoard
 from Chesspiece import Chesspiece
 
+self.score = 0
+self.board = ChessBoard() #generate fresh board
+
+# Best fit heuristic to evaluate numerical position
+def update(self,board):
+    self.score = board.evaluateMaterial()
+    
